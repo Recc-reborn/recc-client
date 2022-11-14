17a4bc84cb6f09e05c0419a3a14b0b49402799da
roblescristian4722 <roblescristian4722@gmail.com>
2022-11-14 08:27:41 -0600

    removes duplicated tags

--- app/src/main/java/com/recc/recc_client/layout/recyclerview/view_holders/PlaylistItemViewHolder.kt
+++ app/src/main/java/com/recc/recc_client/layout/recyclerview/view_holders/PlaylistItemViewHolder.kt
@@ -3,6 +3,7 @@ package com.recc.recc_client.layout.recyclerview.view_holders
 import android.app.ActionBar.LayoutParams
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.children
 import com.bumptech.glide.Glide
 import com.recc.recc_client.databinding.FragmentPlaylistItemBinding
 import com.recc.recc_client.layout.home.HomeViewModel
@@ -25,6 +26,12 @@ class PlaylistItemViewHolder(
 
         // TODO: Use real data
         val genres = GenreFactory.generate(5)
+        // Removes existing tags so that won't have to worry about duplicates
+        binding.flTags.children.forEach {
+            if (it is TextView) {
+                it.text = ""
+            }
+        }
         for (genre in genres) {
             val textView = TextView(binding.root.context)
             val layoutParams = LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
