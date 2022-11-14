74f417f9c2e051c7f0728753cae7338b392159b4
roblescristian4722 <roblescristian4722@gmail.com>
2022-11-14 08:11:52 -0600

    Playlists are shown correctly on Home using MockApi data

--- app/src/main/java/com/recc/recc_client/layout/recyclerview/view_holders/PlaylistItemViewHolder.kt
+++ app/src/main/java/com/recc/recc_client/layout/recyclerview/view_holders/PlaylistItemViewHolder.kt
@@ -1,9 +1,14 @@
 package com.recc.recc_client.layout.recyclerview.view_holders
 
+import android.app.ActionBar.LayoutParams
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.recc.recc_client.databinding.FragmentPlaylistItemBinding
 import com.recc.recc_client.layout.home.HomeViewModel
 import com.recc.recc_client.layout.recyclerview.presenters.PlaylistPresenter
+import com.recc.recc_client.utils.Alert
+import com.recc.recc_client.utils.GenreFactory
 
 class PlaylistItemViewHolder(
     private val binding: FragmentPlaylistItemBinding,
@@ -13,9 +18,20 @@ class PlaylistItemViewHolder(
     fun bind(presenter: PlaylistPresenter) {
         binding.tvCreatedAt.text = presenter.createdAt
         binding.tvName.text = presenter.name
-        binding.tvTags.text = presenter.tags.joinToString { "$it " }
         Glide.with(binding.ivImage)
             .load(presenter.image)
             .into(binding.ivImage)
+
+        // TODO: Use real data
+        val genres = GenreFactory.generate(5)
+        for (genre in genres) {
+            val textView = TextView(binding.root.context)
+            val layoutParams = LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+            layoutParams.setMargins(0, 0, 8, 0)
+            textView.layoutParams = layoutParams
+            textView.text = genre
+
+            binding.flTags.addView(textView)
+        }
     }
 }
